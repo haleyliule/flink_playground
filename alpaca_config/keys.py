6cc706ef1c9fb95f1c8e59a4a86488c344b6bd6a
H L <haleyliule@gmail.com>
2025-04-24 11:26:01 -0400

    mask api keys

--- alpaca_config/keys.py
+++ alpaca_config/keys.py
@@ -1,7 +1,13 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 config = {
-'key_id': 'PK1PXS0AEH19L4PCQD42',
-'secret_key': 'uJIkZlzg9PpKofOCWQ1XbTus9PQJibaztRlyM9qR',
+'key_id': os.getenv('key_id'),
+'secret_key': os.getenv('secret_key'), 
 'redpanda_brokers': ['localhost:9092'],
 'base_url': 'https://data.alpaca.markets/v1beta1/',
 'trade_api_base_url':'https://paper-api.alpaca.markets/v2'
 }
+
