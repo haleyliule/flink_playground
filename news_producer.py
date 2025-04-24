8171ed3c91d1b5cadf249bbf7aaf2cd600455089
H L <haleyliule@gmail.com>
2025-04-24 11:14:12 -0400

    adding sentiment analysis score

--- news_producer.py
+++ news_producer.py
@@ -1,7 +1,8 @@
 from kafka import KafkaProducer
 from typing import List 
 import json
-
+from datetime import datetime 
+from utils import get_sentiment
 # from yarl import URL 
 
 from alpaca_config.keys import config 
@@ -39,18 +40,47 @@ def produce_historical_news (
         news=api.get_news(symbol=symbol, 
                           start = start_date,
                           end = end_date,
-                          limit = 10,
+                          limit = 100,
                           sort=Sort.ASC,
                         include_content=False)
-        print(news)
+        
+        for i, row in enumerate(news):
+            article = row._raw
+            should_proceed= any(term in article['headline'] for term in symbol
+                                )
+            if not should_proceed:
+                continue 
 
+            timestamp_ms = int(row.created_at.timestamp() * 1000)
+            timestamp = datetime.fromtimestamp(row.created_at.timestamp())
+
+            article['timestamp'] = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+            article['timestamp_ms'] = timestamp_ms
+            article['data-provider'] ='alpaca'
+            article['sentiment'] = get_sentiment(article['headline'])
+            article.pop('symbols')
+            article['symbol'] = symbol
+
+            print(article)
+            try:
+                future =redpanda_client.send(
+                    topic=topic,
+                    key=symbol, 
+                    value=article,
+                    timestamp_ms=timestamp_ms
+                )
+
+                _ = future.get(timeout=10)
+                print(f'sent {i+1} to the topic {topic}')
+            except Exception as e:
+                print(e)
 
 
 if __name__ == '__main__':
     produce_historical_news(
         get_producer(config['redpanda_brokers']), 
         topic='market-news',
-        start_date='2025-01-01',
-        end_date= '2025-04-01',
+        start_date='2025-03-01',
+        end_date= '2025-04-24',
         symbols=['AAPL', 'Apple']
     )
